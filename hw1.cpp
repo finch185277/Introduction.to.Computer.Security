0d9a00ce3721d02f756ab617c776dabae0b99b87
YU-AN CHEN <yuanchen@cs.nctu.edu.tw>
2019-03-26 20:09:32 +0800

    Update hw1.cpp
    auto fill MAC address

--- hw1.cpp
+++ hw1.cpp
@@ -13,12 +13,12 @@
 #include <sys/ioctl.h>  // ioctl
 #include <sys/socket.h> // this for struct sockaddr, socket ,and AF_PACKET
 #include <unistd.h>     //getpid
-#define DESTMAC0 0x90
-#define DESTMAC1 0x94
-#define DESTMAC2 0xe4
-#define DESTMAC3 0xfc
-#define DESTMAC4 0x52
-#define DESTMAC5 0x85
+//#define DESTMAC0 0x90
+//#define DESTMAC1 0x94
+//#define DESTMAC2 0xe4
+//#define DESTMAC3 0xfc
+//#define DESTMAC4 0x52
+//#define DESTMAC5 0x85
 #define query_type 1
 // DNS HEADER Structure
 struct DNS_HEADER {
@@ -79,96 +79,33 @@ void DNSNameFormat(unsigned char *DNS, unsigned char *host) {
   *DNS++ = '\0';
 }
 int main(int argc, char **argv) {
-  char interface_name[] = "enp0s8";
   unsigned char hostname[100] = "google.com";
+  int one = 1;
+  const int *val = &one;
   int sock;
-  sock = socket(AF_PACKET, SOCK_RAW, IPPROTO_RAW);
+
+  sock = socket(AF_INET, SOCK_RAW, IPPROTO_UDP);
   if (sock == -1)
     printf("Socket is down\n");
   else {
-    printf("\nStage1: Create Socket Completed!\n\n");
-    // start of getting interface index
-    struct ifreq ifreq_idx;
-
-    memset(&ifreq_idx, 0, sizeof(ifreq_idx));
-
-    strncpy(ifreq_idx.ifr_name, interface_name,
-            IFNAMSIZ - 1); // if no need to change interface ,5 is enough?
-    printf("\n The interface name is: %s\n", ifreq_idx.ifr_name);
-
-    if ((ioctl(sock, SIOCGIFINDEX, &ifreq_idx)) < 0)
-      printf("\nioctl read index fail\n");
-
-    printf("\nThe network interface index is %d\n", ifreq_idx.ifr_ifindex);
-
-    // get the interface index
-
-    // start of getting MAC address
-    struct ifreq ifreq_mac;
-
-    memset(&ifreq_mac, 0, sizeof(ifreq_mac));
-
-    strncpy(ifreq_mac.ifr_name, interface_name, IFNAMSIZ - 1);
-
-    if ((ioctl(sock, SIOCGIFHWADDR, &ifreq_mac)) < 0)
-      printf("\nioctl fails 2 times\n");
-
-    // get the interface  MAC Address
-
-    //  start of getting interface IP Address
-    struct ifreq ifreq_ip;
-
-    memset(&ifreq_ip, 0, sizeof(ifreq_ip));
-    strncpy(ifreq_ip.ifr_name, interface_name, IFNAMSIZ - 1);
-
-    if (ioctl(sock, SIOCGIFADDR, &ifreq_ip) < 0)
-      printf("\nioctl fails 3 times\n");
-
-    // get the interface IP Address
-
-    // here we construct the ethernet header
     int total = 0;
 
     unsigned char *sendbuff;
     sendbuff = (unsigned char *)malloc(128);
     memset(sendbuff, 0, 128);
 
-    struct ether_header *eth = (struct ether_header *)(sendbuff);
-
-    eth->ether_shost[0] = (unsigned char)(ifreq_mac.ifr_hwaddr.sa_data[0]);
-    eth->ether_shost[1] = (unsigned char)(ifreq_mac.ifr_hwaddr.sa_data[1]);
-    eth->ether_shost[2] = (unsigned char)(ifreq_mac.ifr_hwaddr.sa_data[2]);
-    eth->ether_shost[3] = (unsigned char)(ifreq_mac.ifr_hwaddr.sa_data[3]);
-    eth->ether_shost[4] =
-        (unsigned char)(ifreq_mac.ifr_hwaddr.sa_data[4]); // source mac address
-    eth->ether_shost[5] = (unsigned char)(ifreq_mac.ifr_hwaddr.sa_data[5]);
-    // /*
-    eth->ether_dhost[0] = DESTMAC0;
-    eth->ether_dhost[1] = DESTMAC1;
-    eth->ether_dhost[2] = DESTMAC2;
-    eth->ether_dhost[3] = DESTMAC3;
-    eth->ether_dhost[4] = DESTMAC4;
-    eth->ether_dhost[5] = DESTMAC5;
-    //               we fill destination mac address here */
-    eth->ether_type = htons(ETH_P_IP); // next header is IP header
-    total += sizeof(struct ether_header);
     // construct an ip header
-    struct iphdr *iph =
-        (struct iphdr *)(sendbuff + sizeof(struct ether_header));
+    struct iphdr *iph = (struct iphdr *)(sendbuff);
     iph->ihl = 5;     // ip header length
     iph->version = 4; // ipv4
     iph->tos = 16;    // type Of Service // low delay
     iph->id = htons(21910);
     iph->ttl = 64;
     iph->protocol = 17; // 17 for UDP
-                        // iph->saddr = inet_addr("140.113.65.126");
     iph->saddr = inet_addr(argv[1]);
     iph->daddr = inet_addr(argv[2]);
     total += sizeof(struct iphdr);
-    sendbuff[15] = 0;
-    struct udphdr *uh =
-        (struct udphdr *)(sendbuff + sizeof(struct ether_header) +
-                          sizeof(struct iphdr));
+    struct udphdr *uh = (struct udphdr *)(sendbuff + sizeof(struct iphdr));
 
     uh->source = htons(38210);
     uh->dest = htons(53);
@@ -177,8 +114,7 @@ int main(int argc, char **argv) {
     total += sizeof(struct udphdr);
 
     struct DNS_HEADER *DNS =
-        (struct DNS_HEADER *)(sendbuff + sizeof(struct ether_header) +
-                              sizeof(struct iphdr) + sizeof(udphdr));
+        (struct DNS_HEADER *)(sendbuff + sizeof(struct iphdr) + sizeof(udphdr));
 
     DNS->id = (unsigned short)htons(getpid());
 
@@ -214,17 +150,15 @@ int main(int argc, char **argv) {
     total += sizeof(struct DNS_HEADER);
     printf("\nAfter DNS : %d\n", total);
     unsigned char *q_name =
-        (unsigned char *)(sendbuff + sizeof(struct ether_header) +
-                          sizeof(struct iphdr) + sizeof(udphdr) +
+        (unsigned char *)(sendbuff + sizeof(struct iphdr) + sizeof(udphdr) +
                           sizeof(DNS_HEADER));
     DNSNameFormat(q_name, hostname);
     printf("\n");
 
     total += (strlen((const char *)q_name) + 1);
     struct QUESTION *qinfo;
-    qinfo = (struct QUESTION *)(sendbuff + sizeof(struct ether_header) +
-                                sizeof(struct iphdr) + sizeof(udphdr) +
-                                sizeof(DNS_HEADER) +
+    qinfo = (struct QUESTION *)(sendbuff + sizeof(struct iphdr) +
+                                sizeof(udphdr) + sizeof(DNS_HEADER) +
                                 (strlen((const char *)q_name) + 1));
     qinfo->type = htons(query_type);
     qinfo->qclass = htons(1);
@@ -254,49 +188,31 @@ int main(int argc, char **argv) {
     sendbuff[total++] = Z2;
     sendbuff[total++] = data_length1;
     sendbuff[total++] = data_length2;
-    /*
-                    Add->Name = 0x31;
-                    Add->Type = 0x2923;
-                    Add->UDP_Payload_Size = 0x8794;
-                    Add->HBIER = 0x39;
-                    Add->EDNS0_V = 0x94;
-                    Add->Z = 0x5708;
-                    Add->data_length = 0x9185;
-                    printf("Name: %lu\n",sizeof(Add->Name));
-                    printf("Type: %lu\n",sizeof(Add->Type));
-                    printf("UDP_Payload_Size:
-       %lu\n",sizeof(Add->UDP_Payload_Size)); printf("HBIER:
-       %lu\n",sizeof(Add->HBIER)); printf("EDNS0_V:
-       %lu\n",sizeof(Add->EDNS0_V)); printf("Z: %lu\n",sizeof(Add->Z));
-                    printf("data_length: %lu\n",sizeof(Add->data_length));
-                    total += sizeof(Additional_Records);
-    */
-    uh->len =
-        htons((total - sizeof(struct ether_header) - sizeof(struct iphdr)));
-    iph->tot_len = htons(total - sizeof(struct ether_header));
+
+    uh->len = htons((total - sizeof(struct iphdr)));
+
+    iph->tot_len = htons(total);
 
     iph->check =
-        checksum((unsigned short *)(sendbuff + sizeof(struct ether_header)),
-                 (sizeof(struct iphdr) / 2));
-    unsigned char rec;
-    rec = sendbuff[24];
-    sendbuff[24] = sendbuff[25];
-    sendbuff[25] = rec;
+        checksum((unsigned short *)(sendbuff), (sizeof(struct iphdr) / 2));
     printf("\nAfter udp : %d\n", total);
+    if ((setsockopt(sock, IPPROTO_IP, IP_HDRINCL, val, sizeof(one))) < 0)
+      printf("\nsetsockopt error\n");
 
-    struct sockaddr_ll saddr_ll;
-    saddr_ll.sll_ifindex = ifreq_idx.ifr_ifindex;
-    saddr_ll.sll_halen = ETH_ALEN; // ETH_ALEN is defined 6
-    saddr_ll.sll_addr[0] = DESTMAC0;
-    saddr_ll.sll_addr[1] = DESTMAC1;
-    saddr_ll.sll_addr[2] = DESTMAC2;
-    saddr_ll.sll_addr[3] = DESTMAC3;
-    saddr_ll.sll_addr[4] = DESTMAC4;
-    saddr_ll.sll_addr[5] = DESTMAC5;
+    struct sockaddr_in sin, din;
+    sin.sin_family = AF_INET;
+    din.sin_family = AF_INET;
+
+    sin.sin_port = htons(38210);
+    din.sin_port = htons(53);
+
+    sin.sin_addr.s_addr = inet_addr(argv[1]);
+    din.sin_addr.s_addr = inet_addr(argv[2]);
+
     printf("\nAt last: %d\n", total);
+
     int send_len =
-        sendto(sock, sendbuff, total, 0, (struct sockaddr *)&saddr_ll,
-               sizeof(struct sockaddr_ll));
+        sendto(sock, sendbuff, total, 0, (struct sockaddr *)&sin, sizeof(sin));
     if (send_len < 0)
       printf("\nsendto error\n");
     printf("\n succeed! \n");
